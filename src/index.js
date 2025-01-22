e63aea4b4096aecc3dd1606efbb4341b393b7cc7
bborelli <borelli.bruna@gmail.com>
2025-01-22 13:46:37 -0300

    Injected typewriter effect on JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function lineGenerator(event) {
+  event.preventDefault();
 
+  const lineElement = document.querySelector(".line");
+  lineElement.innerHTML = "";
+
+  const typewriter = new Typewriter(lineElement, {
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+
+  typewriter.typeString("A goofy pick-up line here.").start();
+}
+
+const lineGeneratorElement = document.querySelector("#line-generator");
+lineGeneratorElement.addEventListener("submit", lineGenerator);
